e3428fd3053c8bea643b11ae8e630b5c6b0ba992
Timothy Majarian <tmajaria@broadinstitute.org>
2019-01-29 15:29:08 -0500

    remove black as highlight color

--- R/ggmanhattanTM.R
+++ R/ggmanhattanTM.R
@@ -56,7 +56,7 @@ ggmanhattanTM <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P", gr
   
   grey_vals = rep(c("grey40", "grey70"), 13)
   col_vals = rep(c("#FF0000", "#008B00", "#0000FF", "#454545", "#EE00EE", "#009ACD", "#EE7600"), 5)
-  all.cols <- c("#FF0000" = "#FF0000", "#008B00" = "#008B00", "#0000FF" = "#0000FF", "#454545" = "#454545", "#EE00EE" = "#EE00EE", "#009ACD" = "#009ACD", "#EE7600" = "#EE7600","grey30" = "grey30", "grey60" = "grey60")
+  all.cols <- c("#FF0000" = "#FF0000", "#008B00" = "#008B00", "#0000FF" = "#0000FF", "#454545" = "#454545", "#EE00EE" = "#EE00EE", "#009ACD" = "#009ACD", "#EE7600" = "#EE7600","grey40" = "grey40", "grey70" = "grey70")
   all.alpha <- c("0.3" = 0.3, "1" = 1)
   data$color <- as.factor(data$CHR)
   data$color <- grey_vals[data$CHR]
