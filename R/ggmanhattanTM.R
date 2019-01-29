989929579405faf842ee46b1248c5b1fd865c6f3
Timothy Majarian <tmajaria@broadinstitute.org>
2019-01-29 16:50:32 -0500

    remove black as highlight color

--- R/ggmanhattanTM.R
+++ R/ggmanhattanTM.R
@@ -55,8 +55,10 @@ ggmanhattanTM <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P", gr
   data$y = if (logP) -log10(data$P) else data$P
   
   grey_vals = rep(c("grey40", "grey70"), 13)
-  col_vals = rep(c("#FF0000", "#008B00", "#0000FF", "#ffff00", "#EE00EE", "#009ACD", "#EE7600"), 5)
-  all.cols <- c("#FF0000" = "#FF0000", "#008B00" = "#008B00", "#0000FF" = "#0000FF", "#ffff00" = "#ffff00", "#EE00EE" = "#EE00EE", "#009ACD" = "#009ACD", "#EE7600" = "#EE7600","grey40" = "grey40", "grey70" = "grey70")
+  # col_vals = rep(c("#FF0000", "#008B00", "#0000FF", "#ffff00", "#EE00EE", "#009ACD", "#EE7600"), 5)
+  col_vals <- rep("blue", 30)
+  # all.cols <- c("#FF0000" = "#FF0000", "#008B00" = "#008B00", "#0000FF" = "#0000FF", "#ffff00" = "#ffff00", "#EE00EE" = "#EE00EE", "#009ACD" = "#009ACD", "#EE7600" = "#EE7600","grey40" = "grey40", "grey70" = "grey70")
+  all.cols <- c("blue" = "blue", "grey50" = "grey50", "grey80" = "grey80")
   all.alpha <- c("0.3" = 0.3, "1" = 1)
   data$color <- as.factor(data$CHR)
   data$color <- grey_vals[data$CHR]
@@ -72,7 +74,7 @@ ggmanhattanTM <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P", gr
   plt = ggplot(data) + geom_point(data = base::subset(data, GROUP == "NA"), aes(x, y, color = color)) +
           geom_point(data = base::subset(data, GROUP != "NA"), aes(x, y, color = color)) +
           geom_hline(yintercept = -log10(nominal), linetype = "dashed", color = "red") +
-          geom_hline(yintercept = -log10(significance), linetype = "dashed", color = "blue") +
+          geom_hline(yintercept = -log10(significance), linetype = "dashed", color = "green") +
           scale_x_continuous(breaks = conv$breaks, labels = conv$labels, expand = expand.x) +
           scale_y_continuous(expand = expand.y) +
           theme_base +
