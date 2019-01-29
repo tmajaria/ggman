6854d0f91c738e99b7b95eaa72bb4df8ced0f275
Timothy Majarian <tmajaria@broadinstitute.org>
2019-01-29 14:32:01 -0500

    .

--- R/ggmanhattanTM.R
+++ R/ggmanhattanTM.R
@@ -61,9 +61,10 @@ ggmanhattanTM <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P", gr
 
   data$color <- as.factor(data$CHR)
   data$color <- grey_vals[data$CHR]
+  print(head(data))
 
   data[data$P < nominal & data$GROUP != "NA", "color"] <- col_vals[data[data$P < nominal & data$GROUP != "NA", "chr"]]
-  print("here")
+
   data$color <- as.factor(data$color)
 
   plt = ggplot(data, aes(x, y, color = color)) + geom_point() +
