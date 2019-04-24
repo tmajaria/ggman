90b26ba46961c1de3cc157eb703208adc17b9777
Timothy Majarian <tmajaria@broadinstitute.org>
2019-04-24 10:19:14 -0400

    fix grouped plot

--- R/ggmanhattanGrouped.R
+++ R/ggmanhattanGrouped.R
@@ -1,20 +1,174 @@
+# hg19 chromosome sizes
+.hg19 <<- c(0,           # chr1
+          249250621,
+          492449994,
+          690472424,
+          881626700,
+          1062541960,
+          1233657027,
+          1392795690,
+          1539159712,
+          1680373143,  # chr10
+          1815907890,
+          1950914406,
+          2084766301,
+          2199936179,
+          2307285719,
+          2409817111,
+          2500171864,
+          2581367074,
+          2659444322,
+          2722469842,  # chr20
+          2781598825,
+          2829728720,  # chr22
+          2881033286,  # chrX (w/o PAR)
+          3033334811,  # chrY (w/o PAR)
+          3089739342,  # chrXY (PAR1 & PAR2)
+          3095677412)
+
+.hg19.breaks <<- c(124625310,
+                 370850307,
+                 591461209,
+                 786049562,
+                 972084330,
+                 1148099493,
+                 1313226358,
+                 1465977701,
+                 1609766427,
+                 1748140516,
+                 1883411148,
+                 2017840353,
+                 2142351240,
+                 2253610949,
+                 2358551415,
+                 2454994487,
+                 2540769469,
+                 2620405698,
+                 2690957082,
+                 2752034333,
+                 2805663772,
+                 2855381003,
+                 2957184048,
+                 3061537076,
+                 3092708377,
+                 3095685697)
+
+.hg38 <<- c(0,          #chr1
+          248956422,
+          491149951,
+          689445510,
+          879660065,
+          1061198324,
+          1232004303,
+          1391350276,
+          1536488912,
+          1674883629,   #chr10
+          1808681051,
+          1943767673,
+          2077042982,
+          2191407310,
+          2298451028,
+          2400442217,
+          2490780562,
+          2574038003,
+          2654411288,
+          2713028904,    #chr20
+          2777473071,
+          2824183054,     # chr22
+          2875001522,     
+          3031042417,
+          3088269832,
+          3095677412)
+
+.hg38.breaks <<- c(124478211,
+                  370053186,
+                  590297730,
+                  784552788,
+                  970429194,
+                  1146601314,
+                  1311677290,
+                  1463919594,
+                  1605686270,
+                  1741782340,
+                  1876224362,
+                  2010405328,
+                  2134225146,
+                  2244929169,
+                  2349446622,
+                  2445611390,
+                  2532409282,
+                  2614224646,
+                  2683720096,
+                  2745250988,
+                  2800828062,
+                  2849592288,
+                  2953021970,
+                  3059656124,
+                  3092708377,
+                  3095685697)
+
+.labels <<- c(as.character(seq(22)), "X", "Y", "XY", "MT")
+
+scale_colour_dichromatic = function(values = c("grey30", "grey60")) {
+  requireNamespace('ggplot2')
+  values = rep(values, 13)
+  scale_color_manual(values = values)
+}
+
+scale_colour_traditional = function() {
+  requireNamespace('ggplot2')
+  traditional = rep(c("#000000", "#FF0000", "#008B00", "#0000FF", "#454545", "#EE00EE", "#009ACD", "#EE7600"), 4)
+  scale_color_manual(values = traditional)
+}
+
+theme_publication = function(base_size = 16, base_family = "Helvetica", ...) {
+  requireNamespace('ggplot2')
+  thm = theme_bw(base_size = base_size, base_family = base_family, ...)
+  thm + theme(axis.line.x = element_blank(),
+              panel.background  = element_blank(),
+              panel.grid.major  = element_blank(),
+              panel.grid.minor  = element_blank())
+}
+
+..convert2posXR <- function(chr, bp, chr_pos){
+  n_var = length(bp)
+  pos <- c()
+  for (i in 1:n_var){
+    pos <- c(pos, bp[i] + chr_pos[chr[i]])
+  }
+  return(pos)
+}
+
+.convert2posXR <- function(chr, bp, build) {
+  if (length(chr) != length(bp)) {
+    stop("SIZE DIFFER.");
+  }
+
+  n_chr = length(unique(chr))
+  if (n_chr == 1) {
+    return (list(posX = bp,
+                 breaks = ggplot2::waiver(),
+                 labels = ggplot2::waiver(),
+                 xlabel = paste("Chromosome", chr[1])))
+  }
+
+  max_chr = max(chr)
+  if (build == "hg19") {
+    return (list(posX = ..convert2posXR(chr, bp, .hg19),
+                 breaks = .hg19.breaks[1:max_chr],
+                 labels = .labels[1:max_chr],
+                 xlabel = "Chromosome"))
+  } else {
+    return (list(posX = ..convert2posXR(chr, bp, .hg38),
+                 breaks = .hg38.breaks[1:max_chr],
+                 labels = .labels[1:max_chr],
+                 xlabel = "Chromosome"))
+  }
+}
 
 
-#######
 
-#' Plot a manhattan plot using ggplot2.
-#'
-#' @param data
-#' @param SNP
-#' @param chr
-#' @param bp
-#' @param P
-#' @param logP
-#' @param build
-#' @param theme_base
-#' @param scale_color
-#' @export
-ggmanhattanGrouped <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P", group = "group_id", P_char = NULL, logP = TRUE, build = 'hg19',
+ggmanhattanGrouped <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P", group = "group_id", color = "color", P_char = NULL, logP = TRUE, build = 'hg19',
                         nominal = c(1.0e-5),
                         significance = c(5.0e-8), ylim = NULL,
                         lead_snp = NULL, annotate_snp = NULL,
@@ -25,6 +179,7 @@ ggmanhattanGrouped <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P
                         expand.x = c(0.03, 0.03), expand.y = c(0.03, 0.03)) {
   requireNamespace('ggplot2')
 
+  # initial checks on data
   idx = match(c(SNP, chr, bp, P, group), colnames(data))
   if (sum(is.na(idx)) > 0) {
     stop(paste(c(SNP, chr, bp, P, group)[which(is.na(idx))], "not found.", sep = " "))
@@ -50,29 +205,46 @@ ggmanhattanGrouped <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P
     scale_color = scale_color()
   }
 
-  conv = .convert2posX(data$CHR, data$BP, build)
+  # get absolute positions for variants
+  conv = .convert2posXR(data$CHR, data$BP, build)
   data$x = conv$posX
+
+  # convert to log scale
   data$y = if (logP) -log10(data$P) else data$P
   
-  grey_vals = rep(c("grey70", "grey80"), 13)
-  all.alpha <- c("0.3" = 0.3, "1" = 1)
-  data$color <- as.factor(data$CHR)
-  data$color <- grey_vals[data$CHR]
-  data[!is.na(data$site.col), "color"] <- data[!is.na(data$site.col), "site.col"]
+  # define colors for each chromosome
 
+  # grey scale for non-highlighted groups
+  grey_vals = rep(c("gray30", "gray50"), 20)
 
-  data$color <- as.factor(data$color)
-  data$alpha <- "1"
-  data[data$color %in% grey_vals, "alpha"] <- "0.3"
-  all.cols <- c("#ff9d00" = "#ff9d00","#e900ff" = "#e900ff","#2200ff" = "#2200ff","#00bbff" = "#00bbff","#006b0c" = "#006b0c","#006b6b" = "#006b6b","#5e10a3" = "#5e10a3","#ff0000" = "#ff0000")
+  # set colors based on chromosomes
+  data$COLOR <- as.factor(data$CHR)
+  data$COLOR <- grey_vals[data$CHR]
 
+  # add colors to groups if we have any
   data$GROUP <- factor( data$GROUP, levels =  unique(data$GROUP[order(data$GROUP)]))
+  data[!is.na(data[[color]]), "COLOR"] <- as.character(data[!is.na(data[[color]]), color])
+  data$COLOR <- as.factor(data$COLOR)
 
-  if (nrow(data[data$color %in% all.cols,]) > 0){
+  # make list of all colors for later in ggplot
+  all.cols <- levels(data$COLOR)
+  names(all.cols) <- all.cols
+
+  # add alpha level (this may be removed later)
+  data$alpha <- "1"
+  data[data$COLOR %in% grey_vals, "alpha"] <- "0.3"
+  data$alpha <- as.factor(data$alpha)
+
+  # make list of all colors for later in ggplot
+  all.alpha <- levels(data$alpha)
+  names(all.alpha) <- all.alpha
+
+  ### Plots
+
+  if (!all(levels(data$COLOR) %in% grey_vals)){
     print("Multicolor plot")
-    all.cols <- c("#ff9d00" = "#ff9d00","#e900ff" = "#e900ff","#2200ff" = "#2200ff","#00bbff" = "#00bbff","#006b0c" = "#006b0c","#006b6b" = "#006b6b","#5e10a3" = "#5e10a3","#ff0000" = "#ff0000", "grey70" = "grey70", "grey80" = "grey80")
-    plt = ggplot(data) + geom_point(data = base::subset(data, color %in% c("grey70","grey80")), aes(x, y, color = color), size = .5) +
-          geom_point(data = base::subset(data, !(color %in% c("grey70","grey80"))), aes(x, y, color = color), size = .5) +
+    plt = ggplot(data) + geom_point(data = base::subset(data, COLOR %in% grey_vals), aes(x, y, color = COLOR), size = .5) +
+          geom_point(data = base::subset(data, !(COLOR %in% grey_vals)), aes(x, y, color = COLOR), size = 1) +
           geom_hline(yintercept = -log10(nominal), linetype = "dashed", color = "red") +
           geom_hline(yintercept = -log10(significance), linetype = "dashed", color = "black") +
           scale_x_continuous(breaks = conv$breaks, labels = conv$labels, expand = expand.x) +
@@ -87,13 +259,13 @@ ggmanhattanGrouped <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P
           xlab(conv$xlabel) + ylab(expression(-log[10](italic(P))))
   } else {
     print("Monochromatic plot")
-    plt = ggplot(data) + geom_point(data = base::subset(data, color %in% c("grey70","grey80")), aes(x, y, color = color), size = .5) +
+    plt = ggplot(data) + geom_point(data = base::subset(data, COLOR %in% grey_vals), aes(x, y, color = COLOR), size = .5) +
           geom_hline(yintercept = -log10(nominal), linetype = "dashed", color = "red") +
           geom_hline(yintercept = -log10(significance), linetype = "dashed", color = "black") +
           scale_x_continuous(breaks = conv$breaks, labels = conv$labels, expand = expand.x) +
           scale_y_continuous(expand = expand.y) +
           theme_base +
-          scale_color_manual(values = c("grey70" = "grey70", "grey80" = "grey80")) +
+          scale_color_manual(values = all.cols) +
           scale_alpha_manual(values = all.alpha) + 
           theme(axis.text.x = element_text(size = rel(0.5)), legend.position = "none") +
           theme(axis.text.y = element_text(size = rel(0.5)), legend.position = "none") +
@@ -102,86 +274,5 @@ ggmanhattanGrouped <- function(data, SNP = "SNP", chr = "CHR", bp = "BP", P = "P
           xlab(conv$xlabel) + ylab(expression(-log[10](italic(P))))
   }
   
-
-  # if (length(nom_groups) > 0){
-  # plt = ggplot(data) + geom_point(data = base::subset(data, color %in% c("grey70", "grey80")), aes(x, y, color = color)) +
-  #         geom_point(data = base::subset(data, color == "blue"), aes(x, y, color = color)) +
-  #         geom_hline(yintercept = -log10(nominal), linetype = "dashed", color = "red") +
-  #         geom_hline(yintercept = -log10(significance), linetype = "dashed", color = "black") +
-  #         scale_x_continuous(breaks = conv$breaks, labels = conv$labels, expand = expand.x) +
-  #         scale_y_continuous(expand = expand.y) +
-  #         theme_base +
-  #         scale_color_manual(values = all.cols) +
-  #         scale_alpha_manual(values = all.alpha) + 
-  #         theme(axis.text.x = element_text(size = rel(0.75)), legend.position = "none") +
-  #         xlab(conv$xlabel) + ylab(expression(-log[10](italic(P))))
-  # } else {
-  #   plt = ggplot(data) + geom_point(data = data, aes(x, y, color = color)) +
-  #         geom_hline(yintercept = -log10(nominal), linetype = "dashed", color = "red") +
-  #         geom_hline(yintercept = -log10(significance), linetype = "dashed", color = "black") +
-  #         scale_x_continuous(breaks = conv$breaks, labels = conv$labels, expand = expand.x) +
-  #         scale_y_continuous(expand = expand.y) +
-  #         theme_base +
-  #         scale_color_manual(values = all.cols) +
-  #         scale_alpha_manual(values = all.alpha) + 
-  #         theme(axis.text.x = element_text(size = rel(0.75)), legend.position = "none") +
-  #         xlab(conv$xlabel) + ylab(expression(-log[10](italic(P))))
-  # }
-
-  if (!is.null(lead_snp)) {
-    lead_snp = subset(data, data$SNP %in% lead_snp)
-  }
-
-  if (!is.null(annotate_snp)) {
-    lsnp = if(!is.null(ylim)) subset(lead_snp, y < ylim[2]) else lead_snp
-    plt = plt + geom_text(data = lsnp, aes(label = data$SNP), color = "black", angle = 90, hjust = -0.1)
-  }
-
-  if (!is.null(highlight)) {
-    if (!is.list(highlight)) highlight = list(highlight)
-    for (i in 1:length(highlight)) {
-      plt = plt + geom_point(data = subset(data, data$SNP %in% highlight[[i]]), color = highlight_col[i])
-      if (!is.null(lead_snp)) {
-        lead_snp$color[lead_snp$SNP %in% highlight[[i]]] = i
-      }
-    }
-    lead_snp$color = factor(lead_snp$color, levels = 1:length(highlight))
-  }
-
-
-  if (!is.null(ylim)) {
-    plt = plt + coord_cartesian(ylim = ylim)
-    print(lead_snp)
-    print(ylim)
-    if (!is.null(lead_snp)) {
-      lead_snp = subset(lead_snp, y >= ylim[2])
-      scale_color_highlight = if(!is.null(highlight)) scale_color_manual(values = highlight_col) else scale_color
-      if (is.null(P_char)) {
-        lead_snp$label = if(logP) stringr::str_c(sprintf("\u25BA"), sprintf("%.1e",lead_snp$P), sep = "") else sprintf("\u25BA")
-      } else if (P_char != FALSE) {
-        lead_snp$label = stringr::str_c(sprintf("\u25BA"), lead_snp[[P_char]], sep = "")
-      } else {
-        lead_snp$label = sprintf("\u25BA")
-      }
-      plt_annot = ggplot(lead_snp) +
-                    geom_text(aes(x, 0, color = color, label = label), size = 3, vjust = 0.5, angle = 90) +
-                    scale_x_continuous(limits = c(0, max(data$x)), expand = expand.x) +
-                    scale_color_highlight +
-                    theme_void() + theme(plot.margin = margin(6,6,-2,6), legend.position = "none")
-      g1 = ggplotGrob(plt_annot)
-      g2 = ggplotGrob(plt + theme(plot.margin = margin(-2,6,6,6)))
-      maxWidth = grid::unit.pmax(g1$widths[2:5], g2$widths[2:5])
-      g1$widths[2:5] <- as.list(maxWidth)
-      g2$widths[2:5] <- as.list(maxWidth)
-      g = gridExtra::arrangeGrob(g1, g2, heights = c(1, 11))
-      if (plot.grid) {
-        grid::grid.newpage()
-        grid::grid.draw(g)
-      }
-      return(g)
-    }
-  } else if (!is.null(annotate_snp)) {
-    plt = plt + coord_cartesian(ylim = c(0, ceiling(max(data$y) + 1)))
-  }
   return(plt)
 }
